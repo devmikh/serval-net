6eb104d18d853db2dc907a9d19502de8a92c6e8e
devmikh <mishacyb@gmail.com>
2023-02-16 17:05:51 -0800

    Set up DB connection using mysql package

--- app.ts
+++ app.ts
@@ -1,7 +1,19 @@
 import express from 'express';
+import connection from './config/db';
 const port = 3000;
 
 const app = express();
+
+connection.connect(function(err) {
+    if (err) {
+      console.error('Database connection failed: ' + err.stack);
+      return;
+    }
+  
+    console.log('Connected to database.');
+});
+
+connection.end();
 
 app.get('/', (req, res) => {
     res.send('serval-net2');
