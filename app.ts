b290ea1f8234ad93e57b352199f8fc11a1b97ed4
devmikh <mishacyb@gmail.com>
2023-02-19 18:45:16 -0800

    Test user model by creating a new user

--- app.ts
+++ app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import connection from './config/db';
 const port = 3000;
 
+import User from './src/user/UserModel';
+
 const app = express();
 
 connection.connect(function(err) {
@@ -15,8 +17,13 @@ connection.connect(function(err) {
 
 connection.end();
 
-app.get('/', (req, res) => {
-    res.send('serval-net2');
+app.get('/', async (req, res) => {
+    const newUser = await User.create({
+        name: 'John Doe',
+        email: 'john.doe@example.com',
+        password: 'password123'
+      });
+    res.json({user: newUser});
 });
 
 app.listen(port, () => {
