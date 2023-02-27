7c95169e774ee4b76e0f4517c5ca42b68a2df891
devmikh <mishacyb@gmail.com>
2023-02-27 15:00:47 -0800

    Install passport, restructure folders

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,11 +1,5 @@
-import { Sequelize, Model, DataTypes } from 'sequelize';
-import dotenv from 'dotenv';
-dotenv.config();
-
-const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql'
-});
+import { Model, DataTypes } from 'sequelize';
+import sequelize from '../config/sequelize';
 
 class User extends Model {
     public id!: number;
