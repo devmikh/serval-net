b3d4e2b1b8c552d47a12c96ec3617035f54b6642
devmikh <mishacyb@gmail.com>
2023-04-01 12:18:07 -0700

    Extend route behaviour if user was not found

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -6,7 +6,10 @@ router.get('/users/:id', async (req, res) => {
     const result = await fetchUser(Number(req.params.id));
     if (!result.error) {
         res.status(200).json(result);
-    } else {
+    } else if (result.error === 'user_not_found') {
+        res.status(404).json(result);
+    } 
+    else {
         res.status(500).json(result)
     }
 });
