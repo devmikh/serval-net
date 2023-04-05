5f25601a7fbace162d05f1012974384d8ad2c5c9
devmikh <mishacyb@gmail.com>
2023-04-04 19:20:05 -0700

    Debugging

--- app.ts
+++ app.ts
@@ -42,7 +42,8 @@ app.use(session({
     saveUninitialized: false,
     store: sessionStore,
     cookie: {
-        maxAge: 1000 * 60 * 60 * 24
+        maxAge: 1000 * 60 * 60 * 24,
+        sameSite: 'none'
     }
 }));
 
