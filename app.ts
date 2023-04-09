5b29d643c88f046602e6b9cd961e34123a5e3f53
devmikh <mishacyb@gmail.com>
2023-04-09 13:31:38 -0700

    Remove unnecessary code

--- app.ts
+++ app.ts
@@ -45,9 +45,7 @@ app.use(session({
     cookie: {
         maxAge: 1000 * 60 * 60 * 24,
         secure: true,
-        sameSite: 'none',
-        // domain: 'vercel.app'
-        // httpOnly: true
+        sameSite: 'none'
     }
 }));
 
